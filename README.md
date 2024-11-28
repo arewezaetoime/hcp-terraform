667fff7474699f49869bef67017689fde16cee6a
hristoiliev <hristoiliev.1994@gmail.com>
2024-11-28 15:42:50 +0200

    Editted the vcs workspace config to point to the correct repo

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # hcp-terraform
 
 
-Settings/ Developer /settings hcp-terraform-oauthSettings Developer settings hcp-terraform-oauth
+Settings/ Developer /settings /hcp-terraform-oauth
+Settings Developer settings hcp-terraform-oauth
+
+
+we are creating oauth token when connecting hcp to a git repository
+
--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-# terraform {
-#   required_providers {
-#     tfe = {
-#       version = "~> 0.60.1"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    tfe = {
+      version = "~> 0.60.1"
+    }
+  }
+}
 
 provider "tfe" {
   hostname = "app.terraform.io"
@@ -12,38 +12,38 @@ provider "tfe" {
   organization = "the-funky-terraformers"
 }
 
-resource "tfe_organization" "the-funky-terraformers" {
-  name  = var.organization_name
-  email = "xx@xx.com"
-}
+# resource "tfe_organization" "the-funky-terraformers" {
+#   name  = var.organization_name
+#   email = "xx@xx.com"
+# }
 
 resource "tfe_project" "interview-task-project" {
-  organization = tfe_organization.the-funky-terraformers.name
+  organization = var.organization_name
   name = "interview-task-project"
 }
 
 resource "tfe_workspace" "vcs-parent" {
   name                 = "vcs-ws"
-  organization         = tfe_organization.the-funky-terraformers.name
+  organization         = var.organization_name
   project_id           = tfe_project.interview-task-project.id
   vcs_repo {
     branch             = "main"
-    identifier         = "the-funky-terraformers/hcp-terraform-vcs-repo"
-    oauth_token_id     = tfe_oauth_client.github_oauth_client.oauth_token_id
+    identifier         = "arewezaetoime/hcp-terraform"
+    oauth_token_id     = var.oauth_token_id
   }
 }
 
-resource "tfe_oauth_client" "github_oauth_client" {
-  organization      = var.organization_name
-  api_url           = "https://api.github.com"
-  http_url          = "https://github.com"
-  oauth_token = var.oauth_token_id
-  service_provider  = "github"
-}
+# resource "tfe_oauth_client" "github_oauth_client" {
+#   organization      = var.organization_name
+#   api_url           = "https://api.github.com"
+#   http_url          = "https://github.com"
+#   oauth_token = var.oauth_token_id
+#   service_provider  = "github"
+# }
 
 resource "tfe_workspace" "parent" {
   count = 3
   name                 = "parent-${count.index + 1}"
-  organization         = tfe_organization.the-funky-terraformers.name
+  organization         = var.organization_name
   project_id = tfe_project.interview-task-project.id
 }
