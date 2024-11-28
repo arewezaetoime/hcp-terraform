fa34aced64487b60ec8e07e8aa150d383440d7e4
hristoiliev <hristoiliev.1994@gmail.com>
2024-11-28 11:30:19 +0200

    Add changes related to oauth

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-terraform {
-  required_providers {
-    tfe = {
-      version = "~> 0.60.1"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     tfe = {
+#       version = "~> 0.60.1"
+#     }
+#   }
+# }
 
 provider "tfe" {
   hostname = "app.terraform.io"
@@ -13,22 +13,37 @@ provider "tfe" {
 }
 
 resource "tfe_organization" "the-funky-terraformers" {
-  name  = "the-funky-terraformers"
-  email = "tft@tft.com"
+  name  = var.organization_name
+  email = "xx@xx.com"
 }
 
 resource "tfe_project" "interview-task-project" {
-  organization = tfe_organization.name
+  organization = tfe_organization.the-funky-terraformers.name
   name = "interview-task-project"
 }
 
-resource "tfe_workspace" "parent" {
-  name                 = "parent-ws"
-  organization         = tfe_organization.name
+resource "tfe_oauth_client" "github_oauth_client" {
+  organization      = var.github_organization_name
+  api_url           = "https://api.github.com"
+  http_url          = "https://github.com/arewezaetoime/hcp-terraform"
+  service_provider  = "github"
+  oauth_token_id    = var.oauth_token_id # Pass the token as a variable
+}
+
+resource "tfe_workspace" "vcs-parent" {
+  name                 = "vcs-ws"
+  organization         = tfe_organization.the-funky-terraformers.name
   queue_all_runs       = false
   vcs_repo {
     branch             = "main"
     identifier         = "the-funky-terraformers/vcs-repository"
-    //oauth_token_id     = tfe_oauth_client.test.oauth_token_id
+    oauth_token_id     = var.oauth_token_id
   }
+}
+
+resource "tfe_workspace" "parent" {
+  count = 3
+  name                 = "parent-${count.index + 1}"
+  organization         = tfe_organization.the-funky-terraformers.name
+  project_id = tfe_project.interview-task-project.id
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,20 @@ variable "tfe_token"  {
   type        = string
   sensitive   = true
 }
+
+variable "oauth_token_id" {
+  description = "OAuth token ID for connecting to VCS"
+  type        = string
+}
+
+variable "organization_name" {
+  description = "Organization name"
+  type = string
+  sensitive = false
+}
+
+variable "github_organization_name" {
+  description = "Organization name"
+  type = string
+  sensitive = false
+}
